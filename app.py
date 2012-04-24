54d149e3f650639a515a1e958a06d525617b88eb
Dave Paola <dpaola2@gmail.com>
2012-04-23 22:26:15 -0700

    getattr and readdir for posts namespace

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 PORT = int(os.environ.get("PORT", '5000'))
 DEBUG = os.environ.get('DEBUG', True)
 root = Directory(['posts'])
+posts = Directory(Post.all())
 
 @app.route('/', methods=['GETATTR'])
 def index_getattr():
@@ -19,6 +20,14 @@ def index_getattr():
 @app.route('/', methods=['READDIR'])
 def index_readdir():
     return str(root.readdir())
+
+@app.route('/posts', methods=['GETATTR'])
+def posts_getattr():
+    return str(posts.stat())
+
+@app.route('/posts', methods=['READDIR'])
+def posts_readdir():
+    return str(posts.readdir())
 
 app.debug = DEBUG
 app.run(host='0.0.0.0', port=PORT)
