a618d2b1fc11776fc213584e66460d4192dbf77f
Dave Paola <dpaola2@gmail.com>
2012-04-23 20:45:47 -0700

    removed templates

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from flask import Flask, flash, render_template
 app = Flask(__name__)
 
 PORT = int(os.environ.get("PORT", '5000'))
+DEBUG = os.environ.get('DEBUG', True)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    "Hello, world"
 
+app.debug = DEBUG
 app.run(host='0.0.0.0', port=PORT)
